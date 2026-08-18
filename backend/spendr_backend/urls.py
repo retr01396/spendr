2cc6abe459f439fb2fd61970e364fac74eb94f24
retr01396 <nathansv8c@gmail.com>
2026-08-19 00:11:26 +0530

    Update urls.py

--- backend/spendr_backend/urls.py
+++ backend/spendr_backend/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def healthz(request):
+    return HttpResponse("OK")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('subscriptions.urls')),
+    path('healthz', healthz),
 ]
